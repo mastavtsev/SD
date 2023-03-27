91f17ab4a34593a262f62c5c8e47199c9d0d1b07
mastavtsev <111232997+mastavtsev@users.noreply.github.com>
2023-03-27 20:33:18 +0300

    Add files via upload

---
+++ CounterThread.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class CounterThread extends Thread {
+    public static int count = 0;
+    private int startIndex;
+    private int endIndex;
+
+    private List<String> data;
+
+    public CounterThread(int startIndex, int endIndex, List<String> data) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+        this.data = data;
+    }
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i <= endIndex; i++) {
+            count += data.get(i).length();
+        }
+    }
+}
